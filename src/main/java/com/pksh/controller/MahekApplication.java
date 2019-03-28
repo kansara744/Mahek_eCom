7b73118b5232d095b9a7ac11b2b3d72778f2f75e
Elitecore <hardikkava02@gmail.com>
2019-03-28 16:25:27 +0530

    base

---
+++ src/main/java/com/pksh/controller/MahekApplication.java
@@ -0,0 +1,21 @@
+package com.pksh.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MahekApplication extends SpringBootServletInitializer 
+{
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(MahekApplication.class);
+	}
+	
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(MahekApplication.class, args);
+	}
+
+}
